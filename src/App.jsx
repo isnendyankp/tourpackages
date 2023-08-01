78e1793fb94d63f3b4d6346151c791165a004d55
isnendyankp <isnendyankp@gmail.com>
2023-08-01 10:32:49 +0700

    S7-175:cr8 state Variable isLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading,setIsLoading] = useState(true)
   return <h2>Tours Starter</h2>;
 };
 export default App;
+
+// add progress:
+// cr8 state Variable isLoading 
