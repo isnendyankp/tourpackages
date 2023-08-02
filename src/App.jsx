cda19f3074567042765dd6b14487a594115b5633
isnendyankp <isnendyankp@gmail.com>
2023-08-02 10:02:20 +0700

    S7-175: cr8 base useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [isLoading,setIsLoading] = useState(true)
   const [tours, setTours] = useState([]);
+
+  useEffect(()=>{}, [
+
+  ])
   return <h2>Tours Starter</h2>;
 };
 export default App;
 
 // add progress:
-// cr8 state Variable isLoading
-// cr8 state Variable tours 
+// - cr8 state Variable isLoading
+// - cr8 state Variable tours
+// - cr8 base useEffect 
