b05f5fb97f7b05fe7a2b0b1440b35fd6fa9c7e78
isnendyankp <isnendyankp@gmail.com>
2023-08-03 08:21:05 +0700

    S7-176:Add className Loading on Loading component

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
+  return <div className="loading"></div>;
+};
 
 export default Loading
 
 // - cr8 base Loading component
+// - Add className Loading
