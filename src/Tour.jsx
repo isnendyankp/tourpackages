097c02a5e7614bacb7d1bc710f15d5ca9fe8947b
isnendyankp <isnendyankp@gmail.com>
2023-08-06 16:47:56 +0700

    S7-176:Add article with className single-tour

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Tour = ({ id, image, info, name, price }) => {
-  return <h2>Tour</h2>;
+  return <article className='single-tour'>Tour</article>;
 };
 
 export default Tour
 
 // - cr8 base Tour component
-// Destructured element with parameter id,image,info,name,price
+// - Destructured element with parameter id,image,info,name,price
+// - Add article with className single-tour on return
