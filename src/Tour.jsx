74e1fdf1935f5560849b753cf5ec4acde2e16905
isnendyankp <isnendyankp@gmail.com>
2023-08-06 16:49:51 +0700

    S7-176:Setup image Element structure

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Tour = ({ id, image, info, name, price }) => {
-  return <article className='single-tour'>Tour</article>;
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+    </article>
+  );
 };
 
 export default Tour
@@ -9,3 +13,4 @@ export default Tour
 // - cr8 base Tour component
 // - Destructured element with parameter id,image,info,name,price
 // - Add article with className single-tour on return
+// - Setup image Element structure
